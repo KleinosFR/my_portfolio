9c9af35eb8a8746bcdbf93a1145974e1da6fd847
KleinosFR <kleinos@hotmail.com>
2019-11-11 13:56:40 +0100

    created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Row} from "react-materialize"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faReact} from "@fortawesome/free-brands-svg-icons"
+
+import "./footer.css"
+
+
+function Footer() {
+
+
+    return(
+    <footer className="blue lighten-4 valign-wrapper center-align">
+        <Row className="footerText">
+        Copyrigth Stéphane LAVAUD 2019 - React <FontAwesomeIcon color={"blue"} icon={faReact} size={"lg"} /> - Materialize CSS 
+        </Row>
+    </footer>
+    )
+
+
+}
+
+export default Footer
